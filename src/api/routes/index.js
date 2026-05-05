23adf01af9e550bd933be97859b4d38d0752c860
Milad Gerami <eng.miladgerami@gmail.com>
2026-05-04 18:22:53 -0700

    Rate limit admin routes

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const router = express.Router();
 const adminRoutes = require('./adminRoutes');
 
-router.use('/admin', adminRoutes);
+const adminLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 10,
+  message: { message: 'Too many requests, please try again later' },
+});
+
+router.use('/admin', adminLimiter, adminRoutes);
 
 module.exports = router;
